01be9ae5911b34df433fafebf712a1dfa4082e26
Daniel <daniel.corbellini28@gmail.com>
2025-10-11 19:23:12 -0300

    chore: set up supabase connection

--- src/app/cabins/page.tsx
+++ src/app/cabins/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { getCabins } from "@/app/_lib/data-service";
 
 export default function page() {
+  getCabins().then((data) => console.log(data));
+
   return <div>Cabins Page</div>;
 }
